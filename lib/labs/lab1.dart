dee7618d571c01ee5cae7ffb569998b153e46abb
Mikhail <seemihail@mail.ru>
2023-09-12 16:33:36 +0700

    lab1 refactored

--- lib/labs/lab1.dart
+++ lib/labs/lab1.dart
@@ -3,6 +3,20 @@ import 'package:flutter/services.dart';
 
 const operations = ['+', '-', '*', '/'];
 
+double? calculate(double? first, double? second, String operation) {
+  if (first == null || second == null) {
+    return null;
+  } else {
+    return switch (operation) {
+      '+' => first + second,
+      '-' => first - second,
+      '*' => first * second,
+      '/' => first / second,
+      _ => null,
+    };
+  }
+}
+
 class Lab1 extends StatefulWidget {
   const Lab1({super.key});
 
@@ -19,36 +33,32 @@ class _Lab1State extends State<Lab1> {
   double? result;
 
   calculateResult() {
-    if (firstDigit == null || secondDigit == null) {
-      result = null;
-    } else {
-      result = switch (operation) {
-        '+' => firstDigit! + secondDigit!,
-        '-' => firstDigit! - secondDigit!,
-        '*' => firstDigit! * secondDigit!,
-        '/' => firstDigit! / secondDigit!,
-        String() => null,
-      };
-    }
+    result = calculate(firstDigit, secondDigit, operation);
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Lab 1 David Daniel'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.numberWithOptions(
+    return GestureDetector(
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Лаб 1 Семенов Михаил'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    keyboardType: const TextInputType.numberWithOptions(
                       signed: true,
                       decimal: true,
                     ),
@@ -59,26 +69,32 @@ class _Lab1State extends State<Lab1> {
                       calculateResult();
                     },
                   ),
-                ),
-                const SizedBox(width: 12),
-                DropdownButton<String>(
-                  value: operation,
-                  items: operations
-                      .map((e) => DropdownMenuItem<String>(
-                            child: Text(e),
+                  const SizedBox(height: 12),
+                  DropdownButton<String>(
+                    value: operation,
+                    items: operations
+                        .map(
+                          (e) => DropdownMenuItem<String>(
                             value: e,
-                          ))
-                      .toList(),
-                  onChanged: (value) {
-                    if (value == null) return;
-                    operation = value;
-                    calculateResult();
-                  },
-                ),
-                const SizedBox(width: 12),
-                Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.numberWithOptions(
+                            child:
+                                Text(e, style: const TextStyle(fontSize: 24)),
+                          ),
+                        )
+                        .toList(),
+                    onChanged: (value) {
+                      if (value == null) return;
+                      operation = value;
+                      calculateResult();
+                    },
+                  ),
+                  const SizedBox(height: 12),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    keyboardType: const TextInputType.numberWithOptions(
                       signed: true,
                       decimal: true,
                     ),
@@ -89,15 +105,15 @@ class _Lab1State extends State<Lab1> {
                       calculateResult();
                     },
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 50),
-            Text(
-              'Result : ${result ?? ''}',
-              style: TextStyle(fontSize: 24),
-            ),
-          ],
+                ],
+              ),
+              const SizedBox(height: 50),
+              Text(
+                'Ответ: ${result ?? ''}',
+                style: const TextStyle(fontSize: 24),
+              ),
+            ],
+          ),
         ),
       ),
     );
